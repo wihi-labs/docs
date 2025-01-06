22ff6739f08d98beed53f3cca34b79bda2ed9dc1
Michael Chiu <chiumichael@users.noreply.github.com>
2025-01-05 16:42:24 -0800

    docs revamp: initial commit

---
+++ .vitepress/config.mjs
@@ -0,0 +1,48 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "WiHi Docs",
+  head: [["link", {rel: "icon", type: "image/png", href:"/favicon.png"}]],
+  cleanUrls: true,
+  description: "Documentation for WiHi",
+  themeConfig: {
+    logo: "/logo.png",
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      // { text: 'Home', link: '/' },s
+      { text: 'wihi.cc', link: 'https://wihi.cc' }
+    ],
+
+    sidebar: [
+      {
+        text: 'Introduction',
+        collapsed: true,
+        items: [
+          { text: 'Why WiHi?', link: '/' },
+          { text: 'Technology', link: '/intro/tech' },
+          { text: 'Tokenomics', link: '/intro/tokenomics' }
+        ]
+      },
+      {
+        text: 'API',
+        collapsed: true,
+        items: [
+          { text: 'Overview', link: '/api'},
+          { text: '/stations', link: '/api/stations'},
+          { text: '/users', link: '/api/users'},
+        ]
+      }
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/wihi-labs/' },
+      { icon: 'twitter', link: 'https://x.com/wihiweather' },
+      { icon: 'discord', link: 'https://discord.gg/wihi' }
+    ],
+
+    search: {
+      provider: "local",
+    }
+  }
+})
---
+++ index.md
@@ -0,0 +1,4 @@
+---
+title: Why WiHi?
+outline: deep
+---
